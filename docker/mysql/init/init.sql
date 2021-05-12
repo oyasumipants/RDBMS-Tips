d9ea7f068e72ce5c37fc369ee79109353374ac57
Keigo Ibaraki <keigo.ibaraki@siiibo.com>
2021-05-12 14:05:11 +0900

    fix: init.sql
    SQL文の修正

--- docker/mysql/init/init.sql
+++ docker/mysql/init/init.sql
@@ -1,15 +1,43 @@
-DROP TABLE IF EXISTS `sample`;
+DROP TABLE IF EXISTS order_details;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS customers;
 
-SET CHARACTER_SET_CLIENT = utf8;
-SET CHARACTER_SET_CONNECTION = utf8;
+CREATE TABLE customers (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(100) NOT NULL,
+address VARCHAR(100),
+created_at DATETIME NOT NULL,
+updated_at DATETIME NOT NULL,
+PRIMARY KEY(id)
+);
 
-CREATE TABLE `sample` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `content` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE items (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(100) NOT NULL,
+price INT UNSIGNED NOT NULL,
+created_at DATETIME NOT NULL,
+updated_at DATETIME NOT NULL,
+PRIMARY KEY(id)
+);
 
-LOCK TABLES sample WRITE;
-INSERT INTO `sample` (`id`, `content`) VALUES
-(1,	'SpringWork2021G1');
-UNLOCK TABLES;
+CREATE TABLE orders (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+order_date DATE NOT NULL,
+customer_id INT UNSIGNED NOT NULL,
+created_at DATETIME NOT NULL,
+updated_at DATETIME NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
+
+CREATE TABLE order_details (
+order_id INT UNSIGNED NOT NULL,
+item_id INT UNSIGNED NOT NULL,
+item_quantity INT UNSIGNED NOT NULL,
+created_at DATETIME NOT NULL,
+updated_at DATETIME NOT NULL,
+PRIMARY KEY(order_id,item_id),
+FOREIGN KEY (order_id) REFERENCES orders(id),
+FOREIGN KEY (item_id) REFERENCES items(id)
+);
